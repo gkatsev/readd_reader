d7cb5618ebbd0a736582abea53aa90e08fb20afa
Gary Katsevman <git@gkatsev.com>
2012-03-03 23:58:12 -0500

    Add Version

--- readd_reader.user.js
+++ readd_reader.user.js
@@ -3,6 +3,7 @@
 // @namespace      tag:gkatsev.com,2012-01-03:readd_reader
 // @description    Readd google reader link to google header
 // @include        http://*.google.com/*
+// @version        0.1
 // ==/UserScript==
 
 (function(){
@@ -15,13 +16,15 @@
     , moreLI = more.parentNode
     , ol = more.parentNode.parentNode
 
+
+
   spanb2.classList.add('gbtb2')
   spants.classList.add('gbts')
   spants.innerHTML = 'Reader'
 
   anchor.classList.add('gbzt')
   anchor.id='gb_32'
-  anchor.href='http://www.google.com/reader/?hl=en&tab=wy'
+  anchor.href='//www.google.com/reader/?hl=en&tab=wy'
   anchor.onclick = function(){ gbar.logger.il(1,{t:32}); }
   anchor.onmouseover = function(){ anchor.classList.add('gbzt-hvr') }
   anchor.onmouseout = function(){ anchor.classList.remove('gbzt-hvr') }
