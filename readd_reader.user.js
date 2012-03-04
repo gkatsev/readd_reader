05ccf7d31c69e408c2bcae8fcb00c5d9e753a519
Gary Katsevman <git@gkatsev.com>
2012-03-03 23:54:02 -0500

    Wrap code in an IIFE

--- readd_reader.user.js
+++ readd_reader.user.js
@@ -5,37 +5,39 @@
 // @include        http://*.google.com/*
 // ==/UserScript==
 
-var spanb2 = document.createElement('span')
-  , spants = document.createElement('span')
-  , anchor = document.createElement('a')
-  , li = document.createElement('li')
-  , reader = document.querySelector('#gb_32')
-  , more = document.querySelector('#gbztm')
-  , moreLI = more.parentNode
-  , ol = more.parentNode.parentNode
+(function(){
+  var spanb2 = document.createElement('span')
+    , spants = document.createElement('span')
+    , anchor = document.createElement('a')
+    , li = document.createElement('li')
+    , reader = document.querySelector('#gb_32')
+    , more = document.querySelector('#gbztm')
+    , moreLI = more.parentNode
+    , ol = more.parentNode.parentNode
 
-spanb2.classList.add('gbtb2')
-spants.classList.add('gbts')
-spants.innerHTML = 'Reader'
+  spanb2.classList.add('gbtb2')
+  spants.classList.add('gbts')
+  spants.innerHTML = 'Reader'
 
-anchor.classList.add('gbzt')
-anchor.id='gb_32'
-anchor.href='http://www.google.com/reader/?hl=en&tab=wy'
-anchor.onclick = function(){ gbar.logger.il(1,{t:32}); }
-anchor.onmouseover = function(){ anchor.classList.add('gbzt-hvr') }
-anchor.onmouseout = function(){ anchor.classList.remove('gbzt-hvr') }
+  anchor.classList.add('gbzt')
+  anchor.id='gb_32'
+  anchor.href='http://www.google.com/reader/?hl=en&tab=wy'
+  anchor.onclick = function(){ gbar.logger.il(1,{t:32}); }
+  anchor.onmouseover = function(){ anchor.classList.add('gbzt-hvr') }
+  anchor.onmouseout = function(){ anchor.classList.remove('gbzt-hvr') }
 
-anchor.appendChild(spanb2)
-anchor.appendChild(spants)
+  anchor.appendChild(spanb2)
+  anchor.appendChild(spants)
 
-li.classList.add('gbt')
-li.appendChild(anchor)
+  li.classList.add('gbt')
+  li.appendChild(anchor)
 
-reader.parentNode.removeChild(reader)
+  reader.parentNode.removeChild(reader)
 
-ol.insertBefore(li, moreLI)
+  ol.insertBefore(li, moreLI)
 
-if(/.*google\.com\/reader.*/.test(location)){
-  anchor.classList.add('gbz0l')
-  anchor.classList.add('gbp1')
-}
+  if(/.*google\.com\/reader.*/.test(location)){
+    anchor.classList.add('gbz0l')
+    anchor.classList.add('gbp1')
+  }
+})()
